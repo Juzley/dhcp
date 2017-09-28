430364d131a40a2e254f7fa7dfa43bc671c4d8b4
Juzley <juzley@gmail.com>
2017-09-28 23:44:08 +0100

    Don't set next server address in responses

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ config :dhcp, server_address: {192, 168, 0, 14}
 config :dhcp, gateway_address: {192, 168, 0, 1}
 config :dhcp, dns_address: {192, 168, 0, 1}
 config :dhcp, subnet_mask: {255, 255, 255, 0}
-config :dhcp, min_address: {192, 168, 0, 101}
-config :dhcp, max_address: {192, 168, 0, 255}
+config :dhcp, min_address: {192, 168, 0, 2}
+config :dhcp, max_address: {192, 168, 0, 99}
 config :dhcp, max_lease: 86400
 
